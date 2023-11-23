e55c1d4e1882f36c74ef6cea432f17ea29b5e66c
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2023-11-23 05:25:43 +0000

    Updated Task_A4

--- Week 3 Consolidation/Task_A4.sql
+++ Week 3 Consolidation/Task_A4.sql
@@ -41,6 +41,7 @@ ORDER BY trips DESC
 -- unique bikes borrowed 485
 
 -- What was the average trip duration (rounded to the nearest number of minutes) from this station?
+-- **Also condensed as 1 query**
 SELECT TOP 1
 	s.station_id AS ID,
 	s.name AS Station,
@@ -54,5 +55,12 @@ GROUP BY s.station_id, s.name
 ORDER BY trips DESC
 -- average trips duration 35
 
-2)
+-- 2) Are people in Seattle mad enough to cycle when it snows?
+-- on which dates did it snow in Seattle?
+-- on those snowy days, what was the highest number of cycle trips which took place?
+-- Did those trips take place during the whole day or only at certain hours of the day?
+-- How many of those plucky snow-hardy cyclists were members of the scheme and how many
+-- were short term pass holders?
+
+
 
